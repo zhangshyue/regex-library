b2564cf25e5412bf2b5bb44156ecac4434149900
Shiyue Zhang <zhangs@carleton.edu>
2022-02-15 20:13:21 -0600

    delete folders

--- extraction/target/release/build/memchr-54ef801129736ee9/build_script_build-54ef801129736ee9.d
+++
@@ -1,5 +0,0 @@
-/Users/shiyuezhang/Desktop/Programming/comps/regex-library/extraction/target/release/build/memchr-54ef801129736ee9/build_script_build-54ef801129736ee9: /Users/shiyuezhang/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.4.1/build.rs
-
-/Users/shiyuezhang/Desktop/Programming/comps/regex-library/extraction/target/release/build/memchr-54ef801129736ee9/build_script_build-54ef801129736ee9.d: /Users/shiyuezhang/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.4.1/build.rs
-
-/Users/shiyuezhang/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.4.1/build.rs:
--- extraction/target/release/deps/runner-05e08f0c84f09d81.d
+++
@@ -1,5 +0,0 @@
-/Users/shiyuezhang/Desktop/Programming/comps/regex-library/extraction/target/release/deps/runner-05e08f0c84f09d81: programs/runner/src/main.rs
-
-/Users/shiyuezhang/Desktop/Programming/comps/regex-library/extraction/target/release/deps/runner-05e08f0c84f09d81.d: programs/runner/src/main.rs
-
-programs/runner/src/main.rs:
